cdcb1b25642c76d41861abfc254bd11f046d7922
Lacka8 <8laci8@gmail.com>
2019-05-10 20:54:03 +0200

    Set up i3blocks and some template code for it
    Signed-off-by: Lacka8 <8laci8@gmail.com>

---
+++ .scripts/i3blocks/time_block.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+case $BLOCK_BUTTON in
+  1) echo 'Im left clicked!' ;;
+  3) echo 'Im right clicked!' ;;
+esac
+
+date '+%Y-%m-%d %H:%M:%S'
---
+++ .scripts/template/i3blocks-template.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#case $BLOCK_BUTTON in
+#  1) echo 'I\'m left clicked!' ;;
+#  2) echo 'I\'m middle clicked!' ;; # not tested
+#  3) echo 'I\'m right clicked!' ;;
+#  4) echo 'I\'m scrolled up!' ;;
+#  5) echo 'I\'m scrolled down!' ;;
+#  6) echo 'I\'m scrolled left!' ;;
+#  7) echo 'I\'m scrolled right!' ;;
+#esac
+
+# echo 'My default look...'
